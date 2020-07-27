2a1c549bc6191569ee9b1830a3b21b5683b37198
Belik85 <beletskyidmytro@gmail.com>
2020-07-27 22:45:47 +0200

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+import {Todolist} from './Todolist';
+
+function App() {
+
+    let tasks = [
+        { id: 1, title: "HTML&CSS", isDone: true },
+        { id: 2, title: "JS", isDone: true },
+        { id: 3, title: "ReactJS", isDone: false },
+        { id: 4, title: "GraphQL", isDone: false },
+    ]
+
+    function removeTask (id: number) {
+        tasks = tasks.filter(t => t.id !== id)
+        console.log(tasks)
+    }
+
+
+
+    return (
+        <div className="App">
+            <Todolist
+                title="What to learn"
+                tasks={tasks}
+                removeTask={removeTask}
+            />
+        </div>
+    );
+}
+
+export default App;

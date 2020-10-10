a6346bbd2209d6a6a239fcee5ea94c8a6fa07085
Belik85 <beletskyidmytro@gmail.com>
2020-10-10 22:03:06 +0200

    update the project

---
+++ src/EditableSpan.tsx
@@ -0,0 +1,52 @@
+import React, {ChangeEvent, useState} from "react";
+import {TextField} from "@material-ui/core";
+
+
+type EditableSpanPropsType = {
+    value: string;
+    changeValue: (value: string) => void
+}
+
+function EditableSpan(props: EditableSpanPropsType) {
+
+    let [editMode, setEditMode] = useState<boolean>(false)
+    let [title, setTitle] = useState<string>(props.value)
+
+    const activatedEditMode = () => {
+        setEditMode(true)
+        // props.changeValue(title)
+    }
+
+    const deActivatedEditMode = () => {
+
+        setEditMode(false)
+        props.changeValue(title)
+    }
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.currentTarget.value);
+    };
+
+
+    return (
+    editMode
+        ?
+        <TextField
+            variant={"outlined"}
+            value={title}
+            onBlur={deActivatedEditMode}
+            autoFocus={true}
+            onChange={onChangeHandler}
+        />
+        // <input
+        //     value={title}
+        //     onBlur={deActivatedEditMode}
+        //     autoFocus={true}
+        //     onChange={onChangeHandler}
+        // />
+        : <span onDoubleClick={activatedEditMode}>{props.value}</span>
+
+    )
+}
+
+export default EditableSpan

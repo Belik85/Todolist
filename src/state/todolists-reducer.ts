76499072811fcecb9181a82c31b09b42c41f6b21
Belik85 <beletskyidmytro@gmail.com>
2021-03-25 10:36:04 +0100

    update the project

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -103,10 +103,6 @@ export const ChangeFilterAC = (todoListID: string, filter: FilterValuesType):Cha
 }
 
 
-
-
-
-
 // export const getTodolistAC = (todos: Array<TodoListType>) => {
 //     return {
 //         type: "GET-TODOS",
